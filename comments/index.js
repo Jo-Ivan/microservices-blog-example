08eae3c88fa457f18f84de91e22614a73cc078e5
Jo-Ivan Melgarejo <joivan.melgarejo@gmail.com>
2020-05-16 15:33:14 -0400

    implement event bus store

--- comments/index.js
+++ comments/index.js
@@ -14,7 +14,7 @@ app.get("/posts/:id/comments", (req, res) => {
   res.send(commentsByPostId[req.params.id] || []);
 });
 
-app.post("/posts/:id/comments", (req, res) => {
+app.post("/posts/:id/comments", async (req, res) => {
   const commentId = randomBytes(4).toString("hex");
   const { content } = req.body;
 
@@ -22,7 +22,9 @@ app.post("/posts/:id/comments", (req, res) => {
 
   comments.push({ id: commentId, content, status: "pending" });
 
-  axios.post("http://localhost:4005/events", {
+  commentsByPostId[req.params.id] = comments;
+
+  await axios.post("http://localhost:4005/events", {
     type: "CommentCreated",
     data: {
       id: commentId,
@@ -32,17 +34,37 @@ app.post("/posts/:id/comments", (req, res) => {
     }
   });
 
-  commentsByPostId[req.params.id] = comments;
-
   res.status(201).send(comments);
 });
 
-app.post("/events", (req, res) => {
-  console.log("Event Received", req.body.type);
+app.post("/events", async (req, res) => {
+  console.log("Event Received:", req.body.type);
+
+  const { type, data } = req.body;
+
+  if (type === "CommentModerated") {
+    const { postId, id, status, content } = data;
+    const comments = commentsByPostId[postId];
+
+    const comment = comments.find((comment) => {
+      return comment.id === id;
+    });
+    comment.status = status;
+
+    await axios.post("http://localhost:4005/events", {
+      type: "CommentUpdated",
+      data: {
+        id,
+        status,
+        postId,
+        content
+      }
+    });
+  }
 
   res.send({});
 });
 
 app.listen(4001, () => {
-  console.log("Listening on port 4001");
+  console.log("Listening on 4001");
 });
