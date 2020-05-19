39d44f8037ed5ff20f88dff22888085aa7c36ce4
Jo-Ivan Melgarejo <joivan.melgarejo@gmail.com>
2020-05-18 20:00:52 -0400

    add all services

--- event-bus/index.js
+++ event-bus/index.js
@@ -12,13 +12,13 @@ app.post("/events", (req, res) => {
 
   events.push(event);
 
-  axios.post("http://localhost:4000/events", event);
-  axios.post("http://localhost:4001/events", event);
-  axios.post("http://localhost:4002/events", event);
-  axios.post("http://localhost:4003/events", event);
+  axios.post("http://posts-clusterip-srv:4000/events", event);
+  // axios.post("http://localhost:4001/events", event);
+  // axios.post("http://localhost:4002/events", event);
+  // axios.post("http://localhost:4003/events", event);
 
-  axios.post("http://localhost:4006/events", event);
-  axios.post("http://localhost:4007/events", event);
+  // axios.post("http://localhost:4006/events", event);
+  // axios.post("http://localhost:4007/events", event);
 
   res.send({ status: "OK" });
 });
